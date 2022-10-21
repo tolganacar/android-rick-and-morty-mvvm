2d87b4a4ec643afc41ff5736ad7ef1fcb28f218a
Tolga Nacar <tolga_nacar@outlook.com>
2022-10-21 12:36:50 +0300

    Created bottom navigation bar

--- RickMorty/app/src/main/java/com/tolganacar/rickmorty/view/main/MainActivity.kt
+++ RickMorty/app/src/main/java/com/tolganacar/rickmorty/view/main/MainActivity.kt
@@ -2,11 +2,34 @@ package com.tolganacar.rickmorty.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tolganacar.rickmorty.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
     }
 }
---
+++ RickMorty/app/src/main/java/com/tolganacar/rickmorty/view/rmlocationlist/RMLocationListFragment.kt
@@ -0,0 +1,24 @@
+package com.tolganacar.rickmorty.view.rmlocationlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tolganacar.rickmorty.R
+
+class RMLocationListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_location_list, container, false)
+    }
+
+}
